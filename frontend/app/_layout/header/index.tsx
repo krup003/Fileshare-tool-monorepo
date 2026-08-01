d269b7b181b69360176547b8cbcdf05f6e20616d
krup003 <krupkantesariya000@gmail.com>
2026-08-01 19:15:59 +0530

    Include the GITHUB icon for open souce contribution

--- frontend/app/_layout/header/index.tsx
+++ frontend/app/_layout/header/index.tsx
@@ -2,9 +2,11 @@
 
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { History, MessageSquare } from "lucide-react";
+import { History, MessageSquare, Star } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { FeedbackModal } from "@/components/FeedbackModal";
+import GithubLogo from "@/public/svgs/GithubLogo";
+import Link from "next/link";
 
 const Header = () => {
   const pathname = usePathname();
@@ -49,7 +51,7 @@ const Header = () => {
             <Button
               variant="secondary"
               onClick={handleScrollToSection}
-              className="flex items-center gap-2 text-sm transition h-9 w-9 sm:w-auto sm:px-4 p-0 sm:p-2"
+              className="flex items-center gap-2 text-sm transition h-9 w-9 sm:w-auto sm:px-4 p-0 sm:p-2 cursor-pointer"
             >
               <History className="h-4 w-4 shrink-0" />
               <span className="hidden sm:inline">Share History</span>
@@ -58,12 +60,27 @@ const Header = () => {
           <FeedbackModal>
             <Button
               variant="outline"
-              className="flex items-center gap-2 text-sm transition h-9 w-9 sm:w-auto sm:px-4 p-0 sm:p-2"
+              className="flex items-center gap-2 text-sm transition h-9 w-9 sm:w-auto sm:px-4 p-0 sm:p-2 cursor-pointer"
             >
               <MessageSquare className="h-4 w-4 shrink-0" />
               <span className="hidden sm:inline">Feedback</span>
             </Button>
           </FeedbackModal>
+          <Button variant="outline" asChild>
+            <Link
+              href="https://github.com/krup003/Fileshare-tool-monorepo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2"
+            >
+              <GithubLogo className="h-4 w-4 shrink-0" />
+
+              <div className="flex items-center gap-1 rounded-md px-2 py-0.5 text-sm">
+                <Star className="h-4 w-4 text-[#f5c542]" fill="#f5c542" />
+                <span>0</span>
+              </div>
+            </Link>
+          </Button>
         </div>
       </div>
     </header>
